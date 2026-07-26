5a6c7b9e47f2ef5f1832d9580e3b1c45e751511e
LuluCodes7723 <tolu.adesina123@gmail.com>
2026-07-26 09:22:25 -0400

    Update Terraform provider version and storage account name

---
+++ main.tf
@@ -0,0 +1,34 @@
+
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "~> 5.0"
+    }
+  }
+}
+
+provider "azurerm" {
+  features {}
+}
+
+# Resource Group
+resource "azurerm_resource_group" "rg" {
+  name     = "rg-blob-storage"
+  location = "East US"
+}
+
+# Storage Account
+resource "azurerm_storage_account" "storage" {
+  name                     = "tolustorage2026"
+  resource_group_name      = azurerm_resource_group.rg.name
+  location                 = azurerm_resource_group.rg.location
+  account_tier             = "Standard"
+  account_replication_type = "LRS"
+}
+
+# Blob Container
+resource "azurerm_storage_container" "container" {
+  name                 = "mycontainer"
+  storage_account_name = azurerm_storage_account.storage.name
+}
